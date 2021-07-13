8a5feb762d9fcb649331e728af5ed9ccf5dc5c07
orest.l <orest.b2b@gmail.com>
2021-07-13 15:47:52 +0300

    CRUD Location (Country, Region, City)

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,10 +6,12 @@ use App\Models\Traits\UseSelectBox;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Spatie\Translatable\HasTranslations;
 
 class Region extends Model
 {
+    use HasSlug;
     use HasFactory;
     use HasTranslations;
     use UseSelectBox;
@@ -17,6 +19,15 @@ class Region extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(['title'])
+            //->usingLanguage('uk')
+            ->saveSlugsTo('slug');
     }
 
     public $translatable = [
