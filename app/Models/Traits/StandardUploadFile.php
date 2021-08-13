d7f6881ff7a80b24ba822195de25e0bd54d9c005
Vadox <vadox.dev@gmail.com>
2021-08-13 12:01:49 +0300

    Missing trait file

---
+++ app/Models/Traits/StandardUploadFile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Traits;
+
+trait StandardUploadFile
+{
+    /**
+     * Saves an image without processing or resizing
+     *
+     * @param $file
+     *
+     * @param string $folderStorage the folder in which the image will be stored
+     *
+     * @return string
+     */
+    public function storeFile($file, string $folderStorage)
+    {
+
+        return $file->store($folderStorage, "public");
+    }
+}
