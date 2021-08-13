d1a6885b83d3a2c4ae8893b6250845667934bb46
JackDarkWeb <jacknouatin1@gmail.com>
2021-08-13 22:32:00 +0000

    Add site menu seeder

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class MenuFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Menu::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->realText(50);
+        $slug = Str::slug($title);
+
+        return [
+            //
+            'title' => $title,
+            'slug' => $slug,
+            'description'=>$this->faker->realText(225),
+        ];
+    }
+}
