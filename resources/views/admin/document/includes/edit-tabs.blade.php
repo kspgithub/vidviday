9859cf246dd84e2f7cd54308dfc960b1675173c3
JackDarkWeb <jacknouatin1@gmail.com>
2021-08-13 19:10:29 +0000

    Reseted default the branch dev

--- resources/views/admin/document/includes/edit-tabs.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="btn-group my-4">
-    <a class="btn btn-outline-primary {{routeActiveClass('admin.document.edit')}}" href="{{route('admin.document.edit', $document)}}">@lang('Basic')</a>
-
-</div>
