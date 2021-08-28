2ceef5d6f34ac30eea024436c04c9bdc24d3179f
Cretive Studio <web415@gmail.com>
2021-08-28 10:05:47 +0300

    transport places events realised & some bugs fixed

---
+++ database/migrations/2021_07_05_163345_create_events_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->longText('text')->nullable();
+            $table->longText('short_text')->nullable();
+            $table->text('slug');
+
+            $table->text('seo_h1')->nullable();
+            $table->text('seo_title')->nullable();
+            $table->text('seo_description')->nullable();
+            $table->text('seo_keywords')->nullable();
+
+            $table->boolean('published')->default(1);
+            $table->boolean('indefinite')->default(0);
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
