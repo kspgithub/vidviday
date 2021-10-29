4f7f05f214f26eeb228d3b1958e631eff934df38
Vadox <vadox.dev@gmail.com>
2021-10-29 17:20:50 +0300

    add discounts admin title column

--- resources/views/admin/tour/includes/questions-actions.blade.php
+++ resources/views/admin/tour/includes/questions-actions.blade.php
@@ -6,8 +6,13 @@
        wire:click.prevent="publishItem({{$model->id}})" title="Опублікувати"><i class="fa fa-check"></i></a>
 
 @endif
-<a href="#" class="btn btn-sm btn-outline-danger m-1"
-   wire:click.prevent="blockItem({{$model->id}})" title="Заблокувати"><i class="fa fa-ban"></i></a>
+@if($model->status !== \App\Models\TourQuestion::STATUS_BLOCKED)
+    <a href="#" class="btn btn-sm btn-outline-danger m-1"
+       wire:click.prevent="blockItem({{$model->id}})" title="Заблокувати"><i class="fa fa-lock"></i></a>
+@else
+    <a href="#" class="btn btn-sm btn-outline-warning m-1"
+       wire:click.prevent="publishItem({{$model->id}})" title="Разблокувати"><i class="fa fa-unlock"></i></a>
+@endif
 
 
 <a href="#" class="btn btn-sm btn-outline-danger m-1"
