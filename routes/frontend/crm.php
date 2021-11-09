67c1df21b93f9fd9fd73428ae72759b1826ed288
Vadox <vadox.dev@gmail.com>
2021-11-09 15:23:14 +0200

    integrations

--- routes/frontend/crm.php
+++ routes/frontend/crm.php
@@ -10,7 +10,7 @@ Route::group([
 ], function () {
     Route::post('/contact-update', [CrmController::class, 'contactUpdate'])->name('contact.update');
     Route::post('/deal-update', [CrmController::class, 'dealUpdate'])->name('deal.update');
-    Route::post('/app-handler', [CrmController::class, 'appHandler'])->name('app.handler');
+    Route::any('/app-handler', [CrmController::class, 'appHandler'])->name('app.handler');
     Route::any('/app-install', [CrmController::class, 'appInstall'])->name('app.install');
     Route::get('/app-check-server', [CrmController::class, 'appCheckServer'])->name('app.check-server');
 });
