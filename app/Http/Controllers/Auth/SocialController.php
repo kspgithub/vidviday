434c03a0f8f06e845fccf69718c87566d3f69c58
Vadox <vadox.dev@gmail.com>
2022-01-18 09:35:37 +0200

    log socialite

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -6,7 +6,9 @@ use App\Exceptions\GeneralException;
 use App\Models\User;
 use App\Services\UserService;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
 /**
@@ -34,27 +36,33 @@ class SocialController extends Controller
      */
     public function callback($provider, UserService $userService)
     {
-        $data = Socialite::driver($provider)->user();
-        if (empty($data->email)) {
-            return redirect()->route('auth.login')->withFlashDanger('До цього облікового запису не прив\'язаний емейл.');
-        }
+        try {
+            $data = Socialite::driver($provider)->user();
+            if (empty($data->email)) {
+                return redirect()->route('auth.login')->withFlashDanger('До цього облікового запису не прив\'язаний емейл.');
+            }
 
 //        if (User::where('email', $data->email)->count() === 0) {
 //            return redirect()->route('auth.login')
 //                ->withFlashDanger('Користувача з адресою електронної пошти, прив\'язаною до цього облікового запису, не знайдено.');
 //        }
 
-        $user = $userService->registerProvider($data, $provider);
-        if (!$user) {
+            $user = $userService->registerProvider($data, $provider);
+            if (!$user) {
+                return redirect()->route('auth.login')->withFlashDanger(__('There was a problem connecting to :provider', ['provider' => $provider]));
+            }
+            if ($user->status !== User::STATUS_ACTIVE) {
+                auth()->logout();
+                return redirect()->route('auth.login')->withFlashDanger(__('Your account has been deactivated.'));
+            }
+
+            auth()->login($user);
+
+            return redirect()->route(homeRoute());
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
             return redirect()->route('auth.login')->withFlashDanger(__('There was a problem connecting to :provider', ['provider' => $provider]));
         }
-        if ($user->status !== User::STATUS_ACTIVE) {
-            auth()->logout();
-            return redirect()->route('auth.login')->withFlashDanger(__('Your account has been deactivated.'));
-        }
-        
-        auth()->login($user);
 
-        return redirect()->route(homeRoute());
     }
 }
