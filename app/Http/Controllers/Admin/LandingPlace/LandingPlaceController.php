8e44840464fc646bbdc7b82f1787333c33e24450
Vadox <vadox.dev@gmail.com>
2022-02-18 11:46:49 +0200

    landing filter

--- app/Http/Controllers/Admin/LandingPlace/LandingPlaceController.php
+++ app/Http/Controllers/Admin/LandingPlace/LandingPlaceController.php
@@ -1,13 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\LandingPlace;
 
 use App\Http\Controllers\Controller;
 use App\Models\LandingPlace;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class LandingPlaceController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return View
+     */
     public function index(Request $request)
     {
         //
@@ -15,6 +20,9 @@ class LandingPlaceController extends Controller
         return view('admin.landing-place.index', ['items' => $items]);
     }
 
+    /**
+     * @return View
+     */
     public function create()
     {
         //
