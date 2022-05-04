944f73ee1ecc3ca65c2762c17460904352a42aba
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-05-04 03:25:14 +0300

    Optimize places page

---
+++ resources/js/components/place/PlacesAccordion.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="accordion-all-expand inner-not-expand">
+        <div class="expand-all-button">
+            <div class="expand-all open">Розгорнути все</div>
+            <div class="expand-all close">Згорнути все</div>
+        </div>
+        <div class="accordion type-4 accordions-inner-wrap">
+            <div v-for="country in countries" class="accordion-item">
+                <div class="accordion-title" @click="loadRegions(country)">{{country.text}}<i></i></div>
+
+                <div class="accordion-inner">
+                    <div class="accordion type-2">
+                        <div v-for="region in getRegions(country)" class="accordion-item">
+                            <div class="accordion-title" @click="loadDistricts(region)" v-html="region.text + '<i></i>'"></div>
+
+                            <div class="accordion-inner">
+                                <div class="accordion type-2" style="margin-left: 15px">
+                                    <div v-for="district in getDistricts(region)" class="accordion-item">
+                                        <div class="accordion-title" @click="loadPlaces(district)" v-html="district.text + '<i></i>'"></div>
+
+                                        <div class="accordion-inner">
+                                            <div class="accordion type-2" style="margin-left: 15px">
+                                                <div v-for="place in getPlaces(district)" class="accordion-item">
+                                                    <div class="accordion-title" @click="loadPlace(district, place)" v-html="place.text + '<i></i>'"></div>
+
+                                                    <div class="accordion-inner">
+
+                                                        <template v-if="place.media">
+                                                            <div class="swiper-entry" v-is="'swiper-slider'" :media="place.media"></div>
+                                                            <div class="spacer-xs"></div>
+                                                        </template>
+
+                                                        <div class="text text-md">
+                                                            <p v-html="place.description"></p>
+
+                                                            <a :href="place.url" class="btn btn-read-more text-bold">Більше</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="expand-all-button">
+            <div class="expand-all open">Розгорнути все</div>
+            <div class="expand-all close">Згорнути все</div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import apiClient from "../../services/api";
+
+export default {
+    name: "PlacesAccordion",
+    props: {
+        countries: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            // countries: [],
+            regions: [],
+            districts: [],
+            places: [],
+        }
+    },
+    computed: {
+        getRegions() {
+            return country => {
+                return this.regions[country.value] || []
+            }
+        },
+        getDistricts() {
+            return region => {
+                return this.districts[region.value] || []
+            }
+        },
+        getPlaces() {
+            return district => {
+                return this.places[district.value] || []
+            }
+        },
+    },
+    methods: {
+        async loadRegions(country) {
+            const response = await apiClient.get('/location/regions', {
+                params: {country_id: country.value},
+            }).catch(error => {
+                console.error(error)
+            })
+
+            if (response) {
+                this.regions[country.value] = response.data
+            }
+        },
+        async loadDistricts(region) {
+            const response = await apiClient.get('/location/districts', {
+                params: {region_id: region.value},
+            }).catch(error => {
+                console.error(error)
+            })
+
+            if (response) {
+                this.districts[region.value] = response.data
+            }
+        },
+        async loadPlaces(district) {
+            const response = await apiClient.get('/places/select-box', {
+                params: {district_id: district.value},
+            }).catch(error => {
+                console.error(error)
+            })
+
+            if (response) {
+                this.places[district.value] = response.data.results
+            }
+        },
+        async loadPlace(district, place) {
+            const response = await apiClient.get('/places/find', {
+                params: {place_id: place.id},
+            }).catch(error => {
+                console.error(error)
+            })
+
+            if (response) {
+                let index = this.places[district.value].indexOf(place)
+                this.places[district.value][index] = {
+                    ...response.data,
+                    ...place,
+                }
+            }
+        },
+    }
+}
+</script>
--- resources/views/place/index.blade.php
+++ resources/views/place/index.blade.php
@@ -53,55 +53,57 @@
                         <div class="spacer-xs"></div>
                     </div>
                     <!-- ACCORDIONS CONTENT -->
-                    <div class="accordion-all-expand inner-not-expand">
-                        <div class="expand-all-button">
-                            <div class="expand-all open">Розгорнути все</div>
-                            <div class="expand-all close">Згорнути все</div>
-                        </div>
-                        <div class="accordion type-4 accordions-inner-wrap">
-                            @foreach($countries as $country)
-                                <div class="accordion-item">
-                                    <div class="accordion-title">{{$country->title}}<i></i></div>
-                                    <div class="accordion-inner">
-                                        <div class="accordion type-2">
-                                            @foreach($country->regions as $region)
-                                                <div class="accordion-item">
-                                                    <div class="accordion-title">{{$region->title}}<i></i></div>
-                                                    <div class="accordion-inner">
-                                                        <div class="accordion type-2">
-                                                            @foreach($region->places as $place)
-                                                                <div class="accordion-item">
-                                                                    <div class="accordion-title">{{ $place->title }}<i></i></div>
-                                                                    <div class="accordion-inner">
-                                                                        @if($place->hasMedia())
-                                                                            <div class="swiper-entry" v-is="'swiper-slider'"
-                                                                                 :media='@json($place->getMedia()->map->toSwiperSlide())'
-                                                                            >
-                                                                            </div>
-                                                                            <div class="spacer-xs"></div>
-                                                                        @endif
-                                                                        <div class="text text-md">
-                                                                            {!! $place->text !!}
-                                                                            <a href="{{ $place->url}}"
-                                                                               class="btn btn-read-more text-bold">Більше</a>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            @endforeach
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-                        <div class="expand-all-button">
-                            <div class="expand-all open">Розгорнути все</div>
-                            <div class="expand-all close">Згорнути все</div>
-                        </div>
-                    </div>
+
+                    <places-accordion :countries="{{ json_encode($countries) }}" />
+{{--                    <div class="accordion-all-expand inner-not-expand">--}}
+{{--                        <div class="expand-all-button">--}}
+{{--                            <div class="expand-all open">Розгорнути все</div>--}}
+{{--                            <div class="expand-all close">Згорнути все</div>--}}
+{{--                        </div>--}}
+{{--                        <div class="accordion type-4 accordions-inner-wrap">--}}
+{{--                            @foreach($countries as $country)--}}
+{{--                                <div class="accordion-item">--}}
+{{--                                    <div class="accordion-title" data-id="{{ $country->id }}" data-children-handler="loadRegions">{{$country->title}}<i></i></div>--}}
+{{--                                    <div class="accordion-inner">--}}
+{{--                                        <div class="accordion type-2">--}}
+{{--                                            @foreach($country->regions as $region)--}}
+{{--                                                <div class="accordion-item">--}}
+{{--                                                    <div class="accordion-title">{{$region->title}}<i></i></div>--}}
+{{--                                                    <div class="accordion-inner">--}}
+{{--                                                        <div class="accordion type-2">--}}
+{{--                                                            @foreach($region->places as $place)--}}
+{{--                                                                <div class="accordion-item">--}}
+{{--                                                                    <div class="accordion-title">{{ $place->title }}<i></i></div>--}}
+{{--                                                                    <div class="accordion-inner">--}}
+{{--                                                                        @if($place->hasMedia())--}}
+{{--                                                                            <div class="swiper-entry" v-is="'swiper-slider'"--}}
+{{--                                                                                 :media='@json($place->getMedia()->map->toSwiperSlide())'--}}
+{{--                                                                            >--}}
+{{--                                                                            </div>--}}
+{{--                                                                            <div class="spacer-xs"></div>--}}
+{{--                                                                        @endif--}}
+{{--                                                                        <div class="text text-md">--}}
+{{--                                                                            {!! $place->text !!}--}}
+{{--                                                                            <a href="{{ $place->url}}"--}}
+{{--                                                                               class="btn btn-read-more text-bold">Більше</a>--}}
+{{--                                                                        </div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                </div>--}}
+{{--                                                            @endforeach--}}
+{{--                                                        </div>--}}
+{{--                                                    </div>--}}
+{{--                                                </div>--}}
+{{--                                            @endforeach--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                        <div class="expand-all-button">--}}
+{{--                            <div class="expand-all open">Розгорнути все</div>--}}
+{{--                            <div class="expand-all close">Згорнути все</div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
                     <!-- ACCORDIONS CONTENT END -->
                 </div>
             </div>
