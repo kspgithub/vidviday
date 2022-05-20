5ed5506a2485723fe37bd2eaa9ffe10e521ffd91
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-05-20 16:18:28 +0300

    Page Seo Text

--- app/View/Components/Page/Regulations.php
+++ app/View/Components/Page/Regulations.php
@@ -2,18 +2,41 @@
 
 namespace App\View\Components\Page;
 
+use App\Models\HtmlBlock;
+use App\Models\Page;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 
 class Regulations extends Component
 {
+    public Page $page;
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Page $page)
     {
         //
+        $this->page = $page;
+    }
+
+    protected function injectHtmlBlocks()
+    {
+        $text = $this->page->text;
+        preg_match_all("/\{\{([^\}\}]*)\}\}/", $text, $matches);
+        if($matches[0] && $matches[1]) {
+            foreach ($matches[1] as $i => $slug) {
+                $htmlBlock = HtmlBlock::query()->where('slug', $slug)->first();
+                $html = '';
+                if($htmlBlock) {
+                    $html = $htmlBlock->text;
+                }
+                $text = Str::replace($matches[0][$i], $html, $text);
+            }
+        }
+        $this->page->text = $text;
     }
 
     /**
@@ -23,6 +46,8 @@ class Regulations extends Component
      */
     public function render()
     {
+        $this->injectHtmlBlocks();
+
         return view('components.page.regulations');
     }
 }
