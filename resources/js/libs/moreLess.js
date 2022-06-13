217d4cab88c347f07de1b5cffda476cb8e5415ab
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-06-13 23:16:23 +0300

    Translatable html blocks show more less

---
+++ resources/js/libs/moreLess.js
@@ -0,0 +1,87 @@
+;(function (window, document) {
+    var config = {
+        textMore: '...Show more',
+        textLess: '...Show less',
+    }
+
+    var setHeight = function (element, height) {
+        if (!element) {;
+            return false;
+        }
+        else {
+            var elementHeight = parseInt(window.getComputedStyle(element, null).height, 10),
+                toggleButton = document.createElement('a'),
+                text = document.createTextNode(config.textMore),
+                parent = element.parentNode;
+
+            toggleButton.src = '#';
+            toggleButton.className = 'show-more';
+            toggleButton.style.float = 'right';
+            toggleButton.style.paddingRight = '15px';
+            toggleButton.appendChild(text);
+
+            parent.insertBefore(toggleButton, element.nextSibling);
+
+            element.setAttribute('data-fullheight', elementHeight);
+            element.style.height = height;
+            return toggleButton;
+        }
+    }
+
+    var toggleHeight = function (element, height) {
+        if (!element) {
+            return false;
+        }
+        else {
+            var full = element.getAttribute('data-fullheight'),
+                currentElementHeight = parseInt(element.style.height, 10);
+
+            element.style.height = full == currentElementHeight ? height : full + 'px';
+        }
+    }
+
+    var toggleText = function (element) {
+        if (!element) {
+            return false;
+        }
+        else {
+            var text = element.firstChild.nodeValue;
+            element.firstChild.nodeValue = text == config.textMore ? config.textLess : config.textMore;
+        }
+    }
+
+
+    var applyToggle = function(elementHeight){
+        'use strict';
+        return function(){
+            toggleHeight(this.previousElementSibling, elementHeight);
+            toggleText(this);
+        }
+    }
+
+
+    var modifyDomElements = function(className, elementHeight, options = {}){
+
+        config.textMore = options.textMore || config.textMore
+        config.textLess = options.textLess || config.textLess
+
+        var elements = document.getElementsByClassName(className);
+        var toggleButtonsArray = [];
+
+
+
+        for (var index = 0, arrayLength = elements.length; index < arrayLength; index++) {
+            var currentElement = elements[index];
+            var toggleButton = setHeight(currentElement, elementHeight);
+            toggleButtonsArray.push(toggleButton);
+        }
+
+        for (var index=0, arrayLength=toggleButtonsArray.length; index<arrayLength; index++){
+            toggleButtonsArray[index].onclick = applyToggle(elementHeight);
+        }
+    }
+
+
+    window.moreLess = modifyDomElements
+})(window, document)
+
--- resources/views/components/page/regulations.blade.php
+++ resources/views/components/page/regulations.blade.php
@@ -5,10 +5,40 @@
         <div class="seo-text load-more-wrapp">
             <div class="row">
                 <div class="col-xl-8 offset-xl-2 col-12">
-                    {!! $seoText !!}
+
+                    <div class="shorten-text">
+                        {!! $seoText !!}
+                    </div>
+
                 </div>
             </div>
         </div>
     </div>
     <div class="spacer spacer-lg"></div>
 </div>
+
+<style>
+    .seo-text-wrapper {
+
+    }
+
+    .seo-text {
+        overflow: hidden;
+    }
+
+    a.show-more {
+
+    }
+</style>
+@push('after-scripts')
+<script>
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    moreLess('seo-text', '150px', {
+        textMore: '{{ __('Показати більше') }}',
+        textLess: '{{ __('Показати менше') }}',
+    });
+})
+</script>
+@endpush()
