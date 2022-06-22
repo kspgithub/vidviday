3bb902f333d6c1c29606b5d948c994ec7262e25a
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-06-22 17:03:06 +0300

    Vacancy email

--- resources/views/vacancy/show.blade.php
+++ resources/views/vacancy/show.blade.php
@@ -60,7 +60,10 @@
                 <div class="col-xl-4 col-12">
                     <!-- SIDEBAR -->
                     <x-page.right-sidebar :pageContent="$pageContent">
-                        <div class="sidebar-item" id="vacancy-form" v-is="'vacancy-form'"></div>
+                        <div class="sidebar-item" id="vacancy-form" v-is="'vacancy-form'"
+                             form-title="{{ __('common.vacancy.form-title-single') }}"
+                             form-sub-title="{{ __('common.vacancy.form-sub-title-single') }}"
+                        ></div>
                     </x-page.right-sidebar>
                     <!-- SIDEBAR END -->
                 </div>
