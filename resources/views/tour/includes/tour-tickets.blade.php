d0535ab4fde2c084fbdbfcf47888e4ae8cbd3dba
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-07-07 17:39:45 +0300

    Tour Tabs active by default

--- resources/views/tour/includes/tour-tickets.blade.php
+++ resources/views/tour/includes/tour-tickets.blade.php
@@ -1,14 +1,16 @@
-<div class="accordion-item">
-    <div class="accordion-title"><span><img src="{{asset('/img/preloader.png')}}"
-                                            data-img-src="{{asset('/icon/tickets.svg')}}"
-                                            alt="tickets"></span>@lang('tours-section.entrance-tickets')<i></i>
-    </div>
-    <div class="accordion-inner">
-        @foreach($tour->groupTourTickets as $ticket)
-            <div class="tickets text text-md">
-                <p><b>{{$ticket->title}}:</b> {!! strip_tags($ticket->text) !!}</p>
-            </div>
-        @endforeach
+@if(in_array('ticket', $tour->active_tabs))
+    <div class="accordion-item">
+        <div class="accordion-title"><span><img src="{{asset('/img/preloader.png')}}"
+                                                data-img-src="{{asset('/icon/tickets.svg')}}"
+                                                alt="tickets"></span>@lang('tours-section.entrance-tickets')<i></i>
+        </div>
+        <div class="accordion-inner">
+            @foreach($tour->groupTourTickets as $ticket)
+                <div class="tickets text text-md">
+                    <p><b>{{$ticket->title}}:</b> {!! strip_tags($ticket->text) !!}</p>
+                </div>
+            @endforeach
 
+        </div>
     </div>
-</div>
+@endif
