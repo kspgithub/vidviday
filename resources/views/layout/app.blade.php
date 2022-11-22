2fb7f1bbd526036326d10d148ab9db75842b7b26
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-11-22 02:54:06 +0200

    Fix i18n

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 
     <!-- CSRF Token -->
-{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
 
     <title>@yield('title', config('app.name', 'Vidviday'))</title>
