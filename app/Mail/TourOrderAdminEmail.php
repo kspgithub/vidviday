739d3667ee992750c6a212b3df387d1e6a3dfa50
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2022-12-30 16:56:41 +0200

    Fix order corporate email notifications

--- app/Mail/TourOrderAdminEmail.php
+++ app/Mail/TourOrderAdminEmail.php
@@ -29,7 +29,7 @@ class TourOrderAdminEmail extends BaseTemplateEmail
         return [
             'order_id' => $this->order->id,
             'departure_date' => fn() => $this->order->start_date?->format('d.m.Y'),
-            'tour_name' => $this->order->tour->title,
+            'tour_name' => $this->order->tour->title ?? 'Корпоративний тур',
             'first_name' => $this->order->first_name,
             'last_name' => $this->order->last_name,
         ];
