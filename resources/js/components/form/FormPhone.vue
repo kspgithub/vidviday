78cf7b604a966f486b28fdad48762b146347e7b7
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-21 17:29:29 +0200

    Fix phone input

--- resources/js/components/form/FormPhone.vue
+++ resources/js/components/form/FormPhone.vue
@@ -94,11 +94,11 @@ export default {
             const updateMask = () => {
                 const countryData = intl.value.getSelectedCountryData()
                 const placeholder = field.inputRef.value.placeholder
-                const length = field.inputRef.value.placeholder.replaceAll(/\D+/g, '').length
+                const length = placeholder.replaceAll(/\D+/g, '').length
                 const mask = placeholder.replace(/\d/g, '9')
                 field.inputRef.value.mask = mask
-                Inputmask(mask).mask(field.inputRef.value);
-                field.innerValue.value = '+' + countryData.dialCode + fullValue.value?.substr(-length)
+                // Inputmask(mask).mask(field.inputRef.value);
+                fullValue.value = fullValue.value?.substr(-length)
             }
 
             field.inputRef.value.addEventListener('countrychange', (e) => {
@@ -114,16 +114,22 @@ export default {
             get() {
                 if (intl.value) {
                     const countryData = intl.value.getSelectedCountryData()
-                    const getNumber = intl.value.getNumber()
+                    const placeholder = field.inputRef.value.placeholder
+                    const length = placeholder.replaceAll(/\D+/g, '').length
                     const regex = new RegExp('^' + countryData.dialCode + '(.*)$')
-                    return field.innerValue.value?.replaceAll(/\D+/g, '').replace(regex, '$1')
+                    return field.innerValue.value?.replaceAll(/\D+/g, '').replace(regex, '$1').substr(-length)
                 }
 
-                return field.innerValue.value
+                return field.innerValue.value.substr(-length)
             },
             set(value) {
-                const countryData = intl.value.getSelectedCountryData()
-                field.innerValue.value = '+' + countryData.dialCode + value
+                if (intl.value) {
+                    const countryData = intl.value.getSelectedCountryData()
+                    const placeholder = field.inputRef.value.placeholder
+                    const length = placeholder.replaceAll(/\D+/g, '').length
+                    const regex = new RegExp('^' + countryData.dialCode + '(.*)$')
+                    field.innerValue.value = '+' + countryData.dialCode + value.replace(regex, '$1').substr(-length)
+                }
             },
         })
 
