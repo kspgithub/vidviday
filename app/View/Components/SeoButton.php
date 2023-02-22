4f6e8e06f20183a3f30ae4721b87f3a45a388047
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2023-02-22 23:02:46 +0200

    Add id to seo buttons

--- app/View/Components/SeoButton.php
+++ app/View/Components/SeoButton.php
@@ -7,6 +7,8 @@ use Illuminate\View\Component;
 
 class SeoButton extends Component
 {
+    public ?int $id;
+
     public string $tag;
 
     public string $as;
@@ -15,11 +17,16 @@ class SeoButton extends Component
 
     public array $config;
 
-    public function __construct(string $key, bool $ssr = false, string $tag = 'button', string $as = 'button', ?string $href = null) {
+    public function __construct(string $key, ?int $id = null, bool $ssr = false, string $tag = 'button', string $as = 'button', ?string $href = null) {
         $this->key = $key;
+        $this->id = $id;
         $this->tag = $ssr ? 'component' : ($href ? 'a' : $tag);
         $this->as = $ssr ? 'seo-button' : $as;
         $this->config = app('seo-buttons')->get($key, []);
+        $this->config['id'] = $this->config['id'] ?? $key;
+        if($this->id) {
+            $this->config['id'] .= '-' . $this->id;
+        }
     }
 
     /**
