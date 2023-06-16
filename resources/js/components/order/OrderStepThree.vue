c52dc9d59ec01cc82082e89027363b09f09e4083
kspgithub <74373296+kspgithub@users.noreply.github.com>
2023-06-16 11:42:18 +0300

    payment  validation

--- resources/js/components/order/OrderStepThree.vue
+++ resources/js/components/order/OrderStepThree.vue
@@ -2,7 +2,7 @@
     <div class="tab" :class="{active: active}">
         <div class="row">
             <div class="col-lg-9 col-md-7 col-12">
-                <order-payment-selector/>
+                <order-payment-selector name="payment_type" rules="required"/>
                 <template v-if="group_type !== 1">
                     <order-offer-date/>
                     <div class="spacer-xs"></div>
