fb34dc33319ea4f54c4603c3765ffe4664a24781
kspgithub <74373296+kspgithub@users.noreply.github.com>
2023-06-16 12:16:31 +0300

    act and offerdate for corporates

--- resources/js/components/order/OrderStepThree.vue
+++ resources/js/components/order/OrderStepThree.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-9 col-md-7 col-12">
                 <order-payment-selector name="payment_type" rules="required"/>
-                <template v-if="group_type !== 1">
+                <template v-if="group_type === 1">
                     <order-offer-date/>
                     <div class="spacer-xs"></div>
                 </template>
@@ -12,7 +12,7 @@
                     <div class="col-lg-5 col-12">
                         <order-confirmation/>
 
-                        <order-act v-if="group_type === 0"/>
+                        <order-act />
 
                         <order-comment/>
 
